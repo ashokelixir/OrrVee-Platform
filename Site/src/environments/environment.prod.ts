5ce0b3e633e4aaeb61accbad225aa89f562b0475
ashokelixir <39168421+ashokelixir@users.noreply.github.com>
2021-05-17 11:34:25 +0530

    updated evn for prod

--- Site/src/environments/environment.prod.ts
+++ Site/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   cognito : {
     REGION : "ap-south-1",
-    USER_POOL_ID : "ap-south-1_tiEgeV8vZ",
-    APP_CLIENT_ID : "46egi13kldl08pjserbhqfpu9f"
+    USER_POOL_ID : "ap-south-1_bdoBUpWBE",
+    APP_CLIENT_ID : "3hel7hkrutrvesgtj1evkvrkb"
   }
 };
